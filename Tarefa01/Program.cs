f1c4f4f205a341bbdb90502419ef0beddf28ad8f
gimidia <gimidia@hotmail.com>
2020-12-29 21:10:20 -0300

    Adicionar arquivos de projeto.

---
+++ Tarefa01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tarefa01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("---------- Codigo lista Original ----------");
+			int[] array = { 7, 5, 3, 9, 6, 4, 1 };
+			foreach (int i in array)
+			{
+				Console.WriteLine(i);
+			}
+
+			Console.WriteLine("---------- Codigo Nova lista ----------");
+			int[] arraynew = { 7, 5, 3, 9, 6, 4, 1 };
+			arraynew[3] = 5; //substituir o valor 9 para 5
+			var listnew = new List<int>(arraynew);
+			listnew.Remove(4); //remover o valor 4 da lista
+			arraynew = listnew.ToArray();
+			foreach (int i in arraynew)
+			{
+				Console.WriteLine(i);
+			}
+
+			Console.WriteLine("---------- Codigo Soma Nova lista ----------");
+			int[] arraysum = { 7, 5, 3, 9, 6, 4, 1 };
+			arraysum[3] = 5; //substituir o valor 9 para 5
+			var listsum = new List<int>(arraysum);
+			listsum.Remove(4); //remover o valor 4 da lista
+			arraysum = listsum.ToArray();
+			var sum = 0;
+			for (var i = 0; i < arraysum.Length; i++)
+			{
+				sum += arraysum[i];
+			}
+			Console.WriteLine($"Soma: {sum}");
+		}
+    }
+}
---
+++ Tarefa03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tarefa03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] array = { 2, 7, 11, 15 };
+			var sumResult01 = 0;
+			var sumResult02 = 0;
+
+			sumResult01 = array[0] + array[1];
+			sumResult02 = array[1] + array[3];
+			Console.WriteLine($"Soma Resultado 01: {sumResult01}");
+			Console.WriteLine($"Soma Resultado 02: {sumResult02}");
+		}
+    }
+}
